f68f6169ac4a04cd69b650a5eb88bbee915cc31b
DileepAmrani <amranidileep44@gmail.com>
2023-05-03 20:00:35 +0500

    footer and nav

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,46 +1,80 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+import styles from '@/styles/Navbar.module.css'
 const Navbar = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            {/* <nav className="navbar navbar-expand-lg p-4 d-flex justify-content-between" >
+                <div>
+                    <Image
+                        src="/assets/logo.png"
+                        alt="Vercel Logo"
+                        // className={styles.vercelLogo}
+                        width={100}
+                        height={50}
+                        priority
+                    />
+                </div>
+
+                <div>
+                    <Image
+                        src="/assets/logo.png"
+                        alt="Vercel Logo"
+                        // className={styles.vercelLogo}
+                        width={100}
+                        height={50}
+                        priority
+                    />
+                </div>
+            </nav> */}
+
+            <nav class="navbar navbar-expand-lg p-3">
+                <div class="container-fluid ">
+                    <Image
+                        src="/assets/logo.png"
+                        alt="Vercel Logo"
+                        // className={styles.vercelLogo}
+                        width={100}
+                        height={50}
+                        priority
+                    />
+                    <div>
+                        <div
+                            class="collapse navbar-collapse mr-auto"
+                            id="navbarSupportedContent"
+                        >
+                            <ul class="navbar-nav mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">
+                                        Home
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">
+                                        Message
+                                    </a>
+                                </li>
+                            </ul>
+                            <button className={`${styles.btn_primary} py-2 px-4 fw-bold text-white rounded`} type="submit">
+                                Sign In
+                            </button>
+                        </div>
+                    </div>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
                 </div>
             </nav>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,44 +17,43 @@ export default function Home() {
       <main >
         <Navbar />
         <h1>Body</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <Footer />
-        {/* <main className={`${styles.main} ${inter.className}`}> */}
-        {/* <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div> */}
-
-        {/* <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div> */}
-
-
       </main>
     </>
   )
