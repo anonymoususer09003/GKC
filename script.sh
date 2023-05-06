f349166d4c0233ba6aa2f562bec1342d55f783f6
Ilya <koluplo@gmail.com>
2023-05-06 09:43:34 +0000

    script.sh edited online with Bitbucket

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-(cd /var/www/html/gkcabc-webapp-4ddbbc061b2a && npm run dev)
+(cd /var/www/html/gkcabc-webapp-4ddbbc061b2a && npm install)
+(cd /var/www/html/gkcabc-webapp-4ddbbc061b2a && npm run build)
