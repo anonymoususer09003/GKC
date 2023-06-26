4f44bd500a87c3287255b1c210e51fa51b48dda6
NinoGlonti <glonti.nin@gmail.com>
2023-06-26 20:14:30 +0400

    token gets deleted on every reload

--- src/utils/withAuthorization.js
+++ src/utils/withAuthorization.js
@@ -9,12 +9,12 @@ const withRole = (WrappedComponent, allowedRoles) => {
             const value = JSON.parse(window.localStorage.getItem("gkcAuth"));
             // Check if the user's role is allowed for this route
             const userRole = value?.role; // Fetch the user's role from the session
-            if(!value){
+          /*  if(!value){
                 router.push('/auth/signin');
             }
             if (!allowedRoles.includes(userRole)) {
                 router.push('/unauthorized');
-            }
+            }*/
         }, []);
 
         return <WrappedComponent {...props }
