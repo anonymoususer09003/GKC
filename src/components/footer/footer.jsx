6fbae4db12f6fcfcbaa26b2681d00132110cb3b4
Mokhtaar <mokh.first@gmail.com>
2023-08-10 01:40:38 +0300

    removeFixed

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import styles from "./footer.module.css"
+import React from 'react';
+import styles from './footer.module.css';
 const Footer = () => {
-    return (
-        <>
-            <footer className="postion-fixed fixed bottom-0 w-100">
-                <div className="d-flex justify-content-between align-items-center flex-wrap bg-light p-3">
-                    <div className={`flex-1 d-flex gap-5 ${styles.linksContainer}`}>
-                        <a href="." className="text-decoration-none text-muted fw-bold small">Privicy Policy</a>
-                        <a href="." className="text-decoration-none text-muted fw-bold small">Term of Use</a>
-                    </div>
-                    <div className="flex-1 ">
-                        <p className={`p-0 m-0 text-muted  fw-bold small ${styles.allRights}`}>&#169; 2023 GSK. All Rights Reserved</p>
-                    </div>
-                </div>
-            </footer>
-        </>
-    )
-}
+  return (
+    <>
+      <footer className="postion-fixed bottom-0 w-100">
+        <div className="d-flex justify-content-between align-items-center flex-wrap bg-light p-3">
+          <div className={`flex-1 d-flex gap-5 ${styles.linksContainer}`}>
+            <a
+              href="."
+              className="text-decoration-none text-muted fw-bold small"
+            >
+              Privicy Policy
+            </a>
+            <a
+              href="."
+              className="text-decoration-none text-muted fw-bold small"
+            >
+              Term of Use
+            </a>
+          </div>
+          <div className="flex-1 ">
+            <p
+              className={`p-0 m-0 text-muted  fw-bold small ${styles.allRights}`}
+            >
+              &#169; 2023 GSK. All Rights Reserved
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
