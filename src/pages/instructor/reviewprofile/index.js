308a77dc59f67528c53dc7254ecee07c4da6bf68
GKC abc <balogun@geekkidscode.com>
2023-09-15 17:43:47 +0000

    Page Protection

--- src/pages/instructor/reviewprofile/index.js
+++ src/pages/instructor/reviewprofile/index.js
@@ -11,7 +11,32 @@ import { MdOutlineCelebration } from "react-icons/md";
 import StarRatings from "react-star-ratings";
 import { withRole } from '../../../utils/withAuthorization';
 
+//important imports for protections starts
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+//important imports for protections ends
+
 function ReviewProfile() {
+    //protection starts
+    const nav = useRouter()
+    // checking if user logged in starts
+      if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+        console.log('lol')
+        useEffect(()=>{
+
+          if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+            nav.push('/') 
+          } else{
+            if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
+              nav.push('/')
+            }
+          }
+
+        },[])
+      }
+    // checking if user logged in ends
+
+    //protection ends
   return (
     <>
       <Head>
