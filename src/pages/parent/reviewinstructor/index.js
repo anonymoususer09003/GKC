4f890ae9bfad38c6b3a902c26d919189e52f9a8a
GKC abc <balogun@geekkidscode.com>
2023-09-15 18:55:28 +0000

    Page Protection

--- src/pages/parent/reviewinstructor/index.js
+++ src/pages/parent/reviewinstructor/index.js
@@ -2,8 +2,32 @@ import Review from "@/components/review/review";
 import { ParentNavbar, Footer } from "../../../components";
 import Head from "next/head";
 import { withRole } from "../../../utils/withAuthorization";
+//important imports for protections starts
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+//important imports for protections ends
 
 function ParentInstructorReview() {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Parent') { //here we check if user has role Parent
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   return (
     <>
       <Head>
