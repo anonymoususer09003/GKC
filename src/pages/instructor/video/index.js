b4fbadc500a3896ed232506243c5cfea93dba39f
GKC abc <balogun@geekkidscode.com>
2023-09-15 17:47:59 +0000

    Page Protection

--- src/pages/instructor/video/index.js
+++ src/pages/instructor/video/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import { TutorNavbar, Footer } from "../../../components";
 import { BsFillMicFill } from "react-icons/bs";
@@ -6,8 +6,29 @@ import { GoDeviceCameraVideo } from "react-icons/go";
 // import { FaShare } from "react-icons/fa";
 import JitsiMeetComponent from "@/components/jitsimeet";
 import { withRole } from '../../../utils/withAuthorization';
+import { useRouter } from 'next/router';
 
 function InstructoVideo() {
+    //protection starts
+    const nav = useRouter()
+    // checking if user logged in starts
+      if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+        console.log('lol')
+        useEffect(()=>{
+
+          if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+            nav.push('/') 
+          } else{
+            if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
+              nav.push('/')
+            }
+          }
+
+        },[])
+      }
+    // checking if user logged in ends
+
+    //protection ends
   const user = {
     id: 1,
     name: "Salman",
