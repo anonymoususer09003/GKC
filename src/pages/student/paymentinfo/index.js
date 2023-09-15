3d0bf88195063499f2066def6863cc40b4c812e2
GKC abc <balogun@geekkidscode.com>
2023-09-15 19:11:22 +0000

    Page Protection

--- src/pages/student/paymentinfo/index.js
+++ src/pages/student/paymentinfo/index.js
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import ParentDetail from "../../../components/stripe/PaymentDetail/index";
 import { withRole } from "../../../utils/withAuthorization";
 import StudentLayout from "@/components/common/StudentLayout";
+import { useRouter } from 'next/router';
 
 function CreditCardInfo() {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Student') { //here we check if user has role Student
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   return (
     <>
       <Head>
