943896322026327c833bd229641eb5d242d57cf4
GKC abc <balogun@geekkidscode.com>
2023-09-15 18:20:57 +0000

    Page Protection

--- src/pages/parent/editprofile/index.js
+++ src/pages/parent/editprofile/index.js
@@ -10,6 +10,26 @@ import { useRouter } from 'next/router';
 import { apiClient } from '../../../api/client';
 
 function EditProfile({ userInfo, loading, error, fetchUser }) {
+    //protection starts
+    const nav = useRouter()
+    // checking if user logged in starts
+      if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+        console.log('lol')
+        useEffect(()=>{
+
+          if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+            nav.push('/') 
+          } else{
+            if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
+              nav.push('/')
+            }
+          }
+
+        },[])
+      }
+    // checking if user logged in ends
+
+    //protection ends
   const navigation = useRouter();
 
   const [updated, setUpdated] = useState(false);
