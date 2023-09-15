41f843e6035a0179c8fdc4a103a0493ba1dd0a1f
GKC abc <balogun@geekkidscode.com>
2023-09-15 19:13:49 +0000

    Page Protection

--- src/pages/student/requestinterview/[instructorId].js
+++ src/pages/student/requestinterview/[instructorId].js
@@ -11,6 +11,26 @@ import { apiClient } from "@/api/client";
  
 
 function RequestInterview({ userInfo, loading, error, fetchUser }) {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Student') { //here we check if user has role Student
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   const router = useRouter();
   const { instructorId } = router.query;
   const [time, setTime] = useState(null);
