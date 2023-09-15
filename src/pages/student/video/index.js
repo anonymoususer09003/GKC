f031d7963f005e413903bfbbf88cc74193679f9b
GKC abc <balogun@geekkidscode.com>
2023-09-15 19:21:03 +0000

    Page Protection

--- src/pages/student/video/index.js
+++ src/pages/student/video/index.js
@@ -3,8 +3,29 @@ import Head from "next/head";
 import { Navbar, Footer } from "../../../components";
 import JitsiMeetComponent from "@/components/jitsimeet";
 import { withRole } from '../../../utils/withAuthorization';
+import { useRouter } from 'next/router';
 
 function StudentVideo() {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Student') { //here we check if user has role Student
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   const user = {
     id: 1,
     name: "Nouman",
