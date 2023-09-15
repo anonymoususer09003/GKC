bbd3b3e955f808846b540c76fff2a5b666b1da75
GKC abc <balogun@geekkidscode.com>
2023-09-15 18:04:37 +0000

    Page Protection

--- src/pages/parent/ccinfo/index.js
+++ src/pages/parent/ccinfo/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import { ParentNavbar, Footer } from "../../../components";
 import { useRouter } from "next/router";
@@ -6,6 +6,26 @@ import PaymentForm from "@/components/stripe/PaymentForm";
 import { withRole } from "../../../utils/withAuthorization";
 
 function ParentCCInfo() {
+   //protection starts
+    const nav = useRouter()
+    // checking if user logged in starts
+      if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+        console.log('lol')
+        useEffect(()=>{
+
+          if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+            nav.push('/') 
+          } else{
+            if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
+              nav.push('/')
+            }
+          }
+
+        },[])
+      }
+    // checking if user logged in ends
+
+    //protection ends
   const navigation = useRouter();
   const onContinue = () => {
     navigation.push("/");
