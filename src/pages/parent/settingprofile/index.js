72a87e3221d551ca4ae5d4808fa98a8a773f2964
GKC abc <balogun@geekkidscode.com>
2023-09-15 18:57:45 +0000

    Page Protection

--- src/pages/parent/settingprofile/index.js
+++ src/pages/parent/settingprofile/index.js
@@ -9,6 +9,26 @@ import { connect } from 'react-redux';
 import { fetchUser } from '../../../store/actions/userActions';
 
 function ParentSettingProfile({ userInfo, loading, error, fetchUser }) {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Parent') { //here we check if user has role Parent
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   const navigation = useRouter();
   const onEditProfile = () => {
     navigation.push('/student/editprofile');
