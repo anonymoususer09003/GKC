036e28afa607c89f95903401dcd2e67c1b64334c
GKC abc <balogun@geekkidscode.com>
2023-09-15 17:38:03 +0000

    Page Protection

--- src/pages/instructor/reportedstudentparents/index.js
+++ src/pages/instructor/reportedstudentparents/index.js
@@ -1,6 +1,7 @@
 import styles from "@/styles/Navbar.module.css";
 import height from "@/styles/Home.module.css";
 import { useState } from "react";
+import { useEffect } from 'react';
 import { useRouter } from "next/router";
 import StarRatings from "react-star-ratings";
 import { TutorNavbar, Footer } from "../../../components";
@@ -8,6 +9,26 @@ import Head from "next/head";
 import { withRole } from '../../../utils/withAuthorization';
 
 function ReportStudentParent() {
+    //protection starts
+    const nav = useRouter()
+    // checking if user logged in starts
+      if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+        console.log('lol')
+        useEffect(()=>{
+
+          if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+            nav.push('/') 
+          } else{
+            if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
+              nav.push('/')
+            }
+          }
+
+        },[])
+      }
+    // checking if user logged in ends
+
+    //protection ends
   const [showActivation, setShowActivation] = useState(false);
   const instructors = ["John Doe", "Jone Rich", "Katy Long"];
   const [rating1, setRating1] = useState(0);
