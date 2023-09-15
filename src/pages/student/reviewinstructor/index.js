f277afe6f8258699030f20a5254184270dbf0962
GKC abc <balogun@geekkidscode.com>
2023-09-15 19:17:32 +0000

    Page Protection

--- src/pages/student/reviewinstructor/index.js
+++ src/pages/student/reviewinstructor/index.js
@@ -1,6 +1,30 @@
 import ReviewInstructor from "../../../components/student/review/review-instructor";
+//important imports for protections starts
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+//important imports for protections ends
 
 const ReviewInsructorPage = () => {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Student') { //here we check if user has role Student
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   return (
     <div>
       <ReviewInstructor/>
