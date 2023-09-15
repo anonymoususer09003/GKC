726238396f9884d2301f69d192404091c1ca51c4
GKC abc <balogun@geekkidscode.com>
2023-09-15 18:39:35 +0000

    Page Protection change for Parents

--- src/pages/parent/financialreport/index.js
+++ src/pages/parent/financialreport/index.js
@@ -11,26 +11,27 @@ import { withRole } from "../../../utils/withAuthorization";
 import styles from "../../../styles/Home.module.css";
 
 function ParentFinancialReport() {
-    //protection starts
-    const nav = useRouter()
-    // checking if user logged in starts
-      if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
-        console.log('lol')
-        useEffect(()=>{
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
 
-          if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
-            nav.push('/') 
-          } else{
-            if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
-              nav.push('/')
-            }
-          }
-
-        },[])
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Parent') { //here we check if user has role Parent
+          nav.push('/')
+        }
       }
-    // checking if user logged in ends
 
-    //protection ends
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
+
   const navigation = useRouter();
   const onRequestRefund = () => {
     navigation.push("/parent/requestrefund");
