39337452492ab9c3429462ec9bd162fb0ce4e8f6
GKC abc <balogun@geekkidscode.com>
2023-09-15 19:02:56 +0000

    Page Protection

--- src/pages/student/coursepay/index.js
+++ src/pages/student/coursepay/index.js
@@ -10,6 +10,26 @@ import { fetchUser } from "@/store/actions/userActions";
 import SendEmailToParents from "@/services/StudentScheduler/SendEmailToParents";
 import { apiClient } from "@/api/client";
 function StudentRegistrationCCPay() {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Student') { //here we check if user has role Student
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   const navigation = useRouter();
   const dispatch = useDispatch();
 
