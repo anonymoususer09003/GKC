264d71adc66e194641c5eb755c0afd7d550a9d06
GKC abc <balogun@geekkidscode.com>
2023-09-15 18:59:14 +0000

    Page Protection

--- src/pages/parent/video/index.js
+++ src/pages/parent/video/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import { ParentNavbar, Footer } from "../../../components";
 import { BsFillMicFill } from "react-icons/bs";
@@ -6,8 +6,29 @@ import { GoDeviceCameraVideo } from "react-icons/go";
 import { FaShare } from "react-icons/fa";
 import JitsiMeetComponent from "@/components/jitsimeet";
 import { withRole } from '../../../utils/withAuthorization';
+import { useRouter } from 'next/router';
 
 function ParentVideo() {
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Parent') { //here we check if user has role Parent
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   const user = {
     id: 1,
     name: "Femi",
