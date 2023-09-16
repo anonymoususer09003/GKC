85c80ef707c80a7cee148169d854073f2f320ae6
GKC abc <balogun@geekkidscode.com>
2023-09-16 12:41:03 +0000

    Where kids learn to code

--- src/pages/instructor/contactus/index.js
+++ src/pages/instructor/contactus/index.js
@@ -33,7 +33,7 @@ function InstructorContactUs() {
     <>
       <Head>
         <title> Contact Us </title>{' '}
-        <meta name="description" content="Where kids code" />
+        <meta name="description" content="Where kids learn to code" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
