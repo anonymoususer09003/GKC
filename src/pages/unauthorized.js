cbbee73fffabf90b074e6461995e413ad3d6cde7
GKC abc <balogun@geekkidscode.com>
2023-09-16 18:01:39 +0000

    unauthorized.js edited online with Bitbucket

--- src/pages/unauthorized.js
+++ src/pages/unauthorized.js
@@ -29,13 +29,13 @@ const [isLogoin, setIsLogin] = useState(null)
         <title>Unauthorized</title>
         <meta name="description" content="Where kids learn to code" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://gkc-images.s3.amazonaws.com/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <section class="bg-white dark:bg-gray-900 ">
        <div class="container flex items-center min-h-screen px-6 py-12 mx-auto">
         <div>
             <p class="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
-            <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">We can’t find that page</h1>
+            <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">We canï¿½t find that page</h1>
             <p class="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist or has been moved.</p>
 
             <div class="flex items-center mt-6 gap-x-3">
