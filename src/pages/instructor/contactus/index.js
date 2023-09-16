0a0999d0f4ec3d433066e346ea5afd93c9b791c5
GKC abc <balogun@geekkidscode.com>
2023-09-16 12:10:16 +0000

    uncomment page protection

--- src/pages/instructor/contactus/index.js
+++ src/pages/instructor/contactus/index.js
@@ -9,26 +9,26 @@ import { useRouter } from 'next/router';
 //important imports for protections ends
 
 function InstructorContactUs() {
-//    //protection starts
-//    const nav = useRouter()
-//    // checking if user logged in starts
-//      if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
-//        console.log('lol')
-//        useEffect(()=>{
-//
-//          if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
-//            nav.push('/') 
-//          } else{
-//            if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
-//              nav.push('/')
-//            }
-//          }
-//
-//        },[])
-//      }
-//    // checking if user logged in ends
-//
-//    //protection ends
+  //protection starts
+  const nav = useRouter()
+  // checking if user logged in starts
+  if(typeof window !== 'undefined'){ // here we check if global object successfully loaded
+    console.log('lol')
+    useEffect(()=>{
+
+      if(JSON.parse(window.localStorage.getItem('gkcAuth')).role === undefined) { //here we check if user signed in
+        nav.push('/') 
+      } else{
+        if(JSON.parse(window.localStorage.getItem('gkcAuth')).role !== 'Instructor') { //here we check if user has role Instructor
+          nav.push('/')
+        }
+      }
+
+    },[])
+  }
+  // checking if user logged in ends
+
+  //protection ends
   return (
     <>
       <Head>
