64e66d0b78d9b5c2e26af51d156c25e77a8bd4b9
GKC abc <balogun@geekkidscode.com>
2023-09-17 04:24:32 +0000

    unauthorized.js edited online with Bitbucket

--- src/pages/unauthorized.js
+++ src/pages/unauthorized.js
@@ -48,7 +48,7 @@ const [isLogoin, setIsLogin] = useState(null)
                     Take me home
                 </button>
                 : 
-                <button class="bg-primary w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600" onClick={()=> router.push('/auth/signin')}>
+                <button class="bg-primary w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600" onClick={()=> router.push('/auth/sigin')}>
                     Login
                 </button>
                 }
