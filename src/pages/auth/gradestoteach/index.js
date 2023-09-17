5ce70e0f0152b14338a07fc11367a7343656646f
GKC abc <balogun@geekkidscode.com>
2023-09-17 04:05:41 +0000

    Removed favicon https

--- src/pages/auth/gradestoteach/index.js
+++ src/pages/auth/gradestoteach/index.js
@@ -48,7 +48,7 @@ export default function RegistrationGrade() {
         <title>Auth | Grades to Teach</title>
         <meta name="description" content="Where kids learn to code" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://gkc-images.s3.amazonaws.com/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container-fluid"
       >
