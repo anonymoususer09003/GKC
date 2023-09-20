df140270a41a30ad282cabfa35218c8779957f44
Ilya <koluplo@gmail.com>
2023-09-20 19:39:48 +0000

    script.sh edited online with Bitbucket

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo rm -rf /var/www/html/gkcabc-webapp-4ddbbc061b2a/node_modules/next
 cd /var/www/html/gkcabc-webapp-4ddbbc061b2a && sudo npm install next && sleep 10
 #sudo systemd-run --scope -p MemoryLimit=750M -p CPUQuota=80% npm install
 #sudo systemd-run --scope -p MemoryLimit=750M -p CPUQuota=80% npm run build
