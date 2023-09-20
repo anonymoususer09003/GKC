f12cbf02974bf95c3b366b9f45521de3172aa783
Ilya <koluplo@gmail.com>
2023-09-20 19:44:36 +0000

    script.sh edited online with Bitbucket

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /var/www/html/gkcabc-webapp-4ddbbc061b2a && sudo npm install next && sleep 10
+sudo rm -rf /var/www/html/gkcabc-webapp-4ddbbc061b2a/node_modules/next
+cd /var/www/html/gkcabc-webapp-4ddbbc061b2a && sudo npm install next@13.3.4 && sleep 10
 #sudo systemd-run --scope -p MemoryLimit=750M -p CPUQuota=80% npm install
 #sudo systemd-run --scope -p MemoryLimit=750M -p CPUQuota=80% npm run build
 sudo pm2 start all
